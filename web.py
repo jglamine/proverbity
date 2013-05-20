61726ef232bae8518c6fa4651cb6505210c027ee
unknown <James@potato.(none)>
2013-05-19 20:46:56 -0400

    database querying implemented

--- web.py
+++ web.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template, Response, redirect
+import db
+import json
 
 app = Flask(__name__)
 
@@ -6,15 +10,17 @@ app = Flask(__name__)
 @app.route('/<int:quote_id>')
 def quote_page(quote_id=None):
     if quote_id is None:
-        return redirect('/0001')
+        return redirect(str(db.get_quote()['id']))
     else:
-        return render_template('quote_page.html', quote_text='this quote',
-                               quote_author='John')
+        quote = db.get_quote(quote_id)
+        return render_template('quote_page.html', quote_text=quote['text'],
+                               quote_author=quote['author'])
 
 @app.route('/ajax/random_quote.json')
 def ajax_quote():
-    json = '{"text": "Knowledge is gained in many schools.", "author": "Hawaiian Proverb", "id": 17580}'
-    return Response(json, mimetype='application/json')
+    json_data = json.dumps(db.get_quote())
+    #json = '{"text": "Knowledge is gained in many schools.", "author": "Hawaiian Proverb", "id": 17580}'
+    return Response(json_data, mimetype='application/json')
 
 
 if __name__ == '__main__':
